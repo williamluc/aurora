0115c3838b3f57191693c2ca39ec21cabab95bc8
SutantoWidjoyo <sutanto_widjoyo@yahoo.com>
2015-10-30 16:18:16 +0700

    xcvb

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,15 +4,6 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("ganti ah");
-
-			
-		System.out.println("switch loh");
-		
-
-		System.out.println("test ah");
-		System.out.println("switch wil loh");
-		System.out.println("switch wil loh (2)");
-	
 	}
 
 }
