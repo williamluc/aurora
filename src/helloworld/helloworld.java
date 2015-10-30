a943517b35d2b37277421eb7e6d28847c4961c63
SutantoWidjoyo <sutanto_widjoyo@yahoo.com>
2015-10-30 15:45:22 +0700

    dsasadsadsadds

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -3,8 +3,8 @@ package helloworld;
 public class helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hi");
+		System.out.println("dsadadsa");
+
 
 	}
 
