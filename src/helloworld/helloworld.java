e41018545c1d47e222446eeb896b68f17d368039
SutantoWidjoyo <sutanto_widjoyo@yahoo.com>
2015-10-30 16:01:22 +0700

    ggggggg

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -5,12 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("ganti ah");
 
-		System.out.println("switch loh");
-		
-
-		System.out.println("test ah");
-		System.out.println("switch wil loh");
-		
+			
 	
 	}
 
