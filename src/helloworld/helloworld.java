aed2362ea153c90ff5be3eef74ab852716544ab1
SutantoWidjoyo <sutanto_widjoyo@yahoo.com>
2015-11-06 13:52:49 +0700

    sutanto committ

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -3,19 +3,8 @@ package helloworld;
 public class helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("ganti ah");
-
-
-			
-		System.out.println("switch loh");
-		
-
-		System.out.println("test ah");
-		System.out.println("switch wil loh");
-		System.out.println("switch wil loh (2)");
-		System.out.println("switch wil loh (3)");
-		
 		System.out.println("Friday Session");
+		System.out.println("test ");
 	
 	}
 
