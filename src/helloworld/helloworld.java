94e411ec4ddf60137ca473a8a9820997f1762889
sofco <sofco@citrahadi.sofco>
2015-11-10 14:47:13 +0700

    chabnr

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,8 +4,6 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Friday Session");
-		System.out.println("test ");
-		System.out.println("test2 ");
 		
 		
 		System.out.println("test23232131 ");
