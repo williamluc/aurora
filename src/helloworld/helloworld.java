c0d53003bd3e4f010b2c36e691bdd309fad01f3e
sofco <sofco@citrahadi.sofco>
2015-11-13 11:05:32 +0700

    remove some ugly code

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -5,23 +5,6 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Friday Session");
 		
-		
-		System.out.println("test23232131 ");
-		
-		System.out.println("Hello Sutanto, Henri, William and Ricky :-)");
-	
-		System.out.println("Friday Session 2 ");
-		
-		System.out.println("HEY, HEY, EVERYBODY!! I WAS HERE!");
-		
-		System.out.println("SECOND ONES");
-		
-		System.out.println("Friday Session 2 Henri ");
-		
-		System.out.println("Friday Session 2 Henri 2");
-		
-		System.out.println("Friday Session 2 Henri 3");
-		
 	}
 
 }
